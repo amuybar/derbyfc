76e44cfde4ab617e81f45f18a6ecf8eac78d241d
amuybar <amuyunzubq@gmail.com>
2024-03-31 21:33:11 +0300

    update

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -7,50 +7,51 @@ const Results = () => {
   const results = [
     {
       id: 1,
-      image: "images/md.jpg",
-      matchday: "Matchday 1",
-      opponent: "Opponent 1",
+      image: "images/game1.jpg",
+      matchday: "Matchday 6",
+      homeTeam: "Gachie sc ",
+      awayTeam: "Derby Fc",
+      results:"2-1",
+      date: "2024-03-11",
     },
     {
       id: 2,
-      image: "images/md.jpg",
-      matchday: "Matchday 2",
-      opponent: "Opponent 2",
+      image: "images/game1.jpg",
+      matchday: "Matchday 5",
+      homeTeam: "Derby Fc",results:"0-0",
+      awayTeam: " Huruma SC",
+      date: "2024-03-05",
     },
     {
       id: 3,
       image: "images/md.jpg",
-      matchday: "Matchday 3",
-      opponent: "Opponent 3",
-    },
-    {
-      id: 2,
-      image: "images/md.jpg",
-      matchday: "Matchday 2",
-      opponent: "Opponent 2",
-    },
-    {
-      id: 3,
-      image: "images/md.jpg",
-      matchday: "Matchday 3",
-      opponent: "Opponent 3",
+      matchday: "Matchday 4",
+      homeTeam: "LSA Fc ",results:"2-4",
+      awayTeam: "derby fc",
+      date: "2024-02-19",
     },
     // Add more result objects as needed
   ];
 
   return (
-    <><h2>Previous Games</h2><div className="results-container">
-      {results.map((result) => (
-        <div key={result.id} className="result-card">
-          <img src={result.image} alt='' className="result-image" />
-          <div className="result-details">
-            <h3>{result.matchday}</h3>
-            <p>Opponent: {result.opponent}</p>
-            <button className="review-button">Review</button>
+    <div>
+      <h2>Previous Games</h2>
+      <div className="results-container">
+        {results.map((result) => (
+          <div key={result.id} className="result-card">
+            <img src={result.image} alt='' className="result-image" />
+            <div className="result-details">
+              <h3>{result.matchday}</h3>
+              <div className='results'><p>{result.homeTeam}</p>
+              <p className='pr'> {result.results}</p>
+              <p>{result.awayTeam}</p></div>
+              <p>Date: {result.date}</p>
+              <button className="review-button">Review</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div></>
+        ))}
+      </div>
+    </div>
   );
 };
 
